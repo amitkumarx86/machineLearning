390e580e1ae3993ea042c208cce9bc81ebb29682
amitkumarx86 <amitkumarx86@gmail.com>
2017-09-16 09:03:15 +0530

    adding new changes

---
+++ matplotlib/test.py
@@ -0,0 +1,15 @@
+what are categorical variable called in R -> factors
+we can convert string to factors
+
+they are enumeration values.
+factors are just like an attribute type, like the ones we have seen in the classical languages
+
+
+we can not do factor_height[1] = "asdlfkahsd"
+
+
+team1[2,1]
+it means 
+team1["2","Player_name"]
+
+team1[2,] # accessing second row
